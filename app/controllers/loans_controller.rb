f5033db66cb3a1e277c335278b73a2c86295d4c4
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-07-17 08:47:53 -0500

    Add new Loan page. Inventory points to new loan.

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,2 +1,24 @@
 class LoansController < ApplicationController
+  before_action :signed_in_user, only: [:create, :edit, :update, :destroy]
+
+  def new
+    redirect_to root_url, alert: "Please Login" unless signed_in?
+
+    @loan = Loan.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+
+  end
+
+  private
+    def loan_params
+      params.require(:loan).permit(:username)
+    end
 end
