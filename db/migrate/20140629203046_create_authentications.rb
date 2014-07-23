c05e6e94d7bac31f86ada76048f37db5a8439346
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-07-23 17:45:42 -0500

    Update Auth migrations.

--- db/migrate/20140629203046_create_authentications.rb
+++ db/migrate/20140629203046_create_authentications.rb
@@ -1,7 +1,6 @@
 class CreateAuthentications < ActiveRecord::Migration
   def change
 
-    drop_table :authentications
     create_table :authentications do |t|
       t.integer :user_id
       t.string :provider
