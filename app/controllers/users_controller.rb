b565c5c2a2588dae5ae65a595e445a2cc46f8513
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-07-23 21:36:54 -0500

    Sign in users after registration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: "Thanks for signing up!"
     else
 
