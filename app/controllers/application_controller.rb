de65bf59bcc08c3d7d535620a0e58c21eed8d2bb
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-07-28 20:39:09 -0500

    Refactor SessionsHelper into ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,30 @@ class ApplicationController < ActionController::Base
     redirect_to login_url, alert: "Not authorized" if current_user.nil?
   end
 
-  include SessionsHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+
+  # Session Helper Methods
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to login_url, notice: "Please sign in." unless signed_in?
+    end
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
+  helper_method :current_user
+  helper_method :signed_in_user
+  helper_method :signed_in?
+  helper_method :store_location
+
 end
