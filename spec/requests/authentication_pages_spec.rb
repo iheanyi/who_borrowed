116d97c8c72aa478cee2d9e45851bf583456e826
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-08-10 18:38:00 -0500

    Add Authentication Spec.

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Authentication" do
+  subject { page }
+
+  describe "signin page" do
+    before { visit login_path }
+
+    it { should have_content("Login Here") }
+    describe "With invalid information" do
+      before { click_button "Login" }
+
+      it { should have_content("Login Here") }
+      it { should have_selector('div.alert.alert-danger') }
+
+    end
+
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email", with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Login"
+      end
+
+      it { should have_content("User Inventory") }
+      it { should have_link(user.username, href: "#") }
+      it { should have_link('Logout', href: logout_path) }
+      it { should_not have_link('Login', href: login_path) }
+    end
+  end
+end
